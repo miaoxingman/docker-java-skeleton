d32f34e2458f6bfe07e969d30f955b03242818fc
miaoxingman <miaoxingman@gmail.com>
2017-10-15 13:55:35 +0800

    Refactor the test.

---
+++ src/test/java/com/miaoxingman/docker/client/test/AbstractDockerClientTest.java
@@ -0,0 +1,57 @@
+package com.miaoxingman.docker.client.test;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.ITestResult;
+
+import com.miaoxingman.docker.client.DockerClient;
+import com.miaoxingman.docker.client.DockerException;
+import com.sun.jersey.api.client.ClientResponse;
+
+public class AbstractDockerClientTest extends Assert {
+
+    public static final Logger LOG = LoggerFactory
+            .getLogger(AbstractDockerClientTest.class);
+
+    protected DockerClient testClient;
+
+    public void beforeTest() throws DockerException {
+        LOG.info("======================= BEFORETEST =======================");
+        LOG.info("Connecting to Testing Docker server");
+        testClient = new DockerClient();
+        LOG.info("Pulling image 'ubuntu'");
+        logResponseStream(testClient.pullImageCmd("ubuntu:latest").exec());
+        LOG.info("======================= END OF BEFORETEST ================\n\n");
+    }
+
+    public void afterTest() {
+        LOG.info("======================= END OF AFTERTEST =================");
+    }
+
+    public void beforeMethod(Method method) {
+        LOG.info(String
+                .format("################################## STARTING %s ##################################",
+                        method.getName()));
+    }
+
+    public void afterMethod(ITestResult result) {
+        LOG.info(String
+                .format("################################## END OF {} ##################################\n",
+                result.getName()));
+    }
+
+    protected String logResponseStream(ClientResponse response) {
+        String responseString;
+        try {
+            responseString = DockerClient.asString(response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        LOG.info("Container log: {}", responseString);
+        return responseString;
+    }
+}
--- src/test/java/com/miaoxingman/docker/client/test/DockerClientTest.java
+++ src/test/java/com/miaoxingman/docker/client/test/DockerClientTest.java
@@ -2,45 +2,64 @@ package com.miaoxingman.docker.client.test;
 
 import static org.testng.Assert.assertTrue;
 
+import java.lang.reflect.Method;
+import java.util.List;
+
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.miaoxingman.docker.client.DockerException;
+import com.miaoxingman.docker.client.model.Image;
 import com.miaoxingman.docker.client.model.Version;
 import com.miaoxingman.docker.client.DockerClient;
 
-public class DockerClientTest extends Assert {
+public class DockerClientTest extends AbstractDockerClientTest {
 
     public static final Logger LOG = LoggerFactory.getLogger(DockerClientTest.class);
-    private DockerClient dockerClient;
 
     @BeforeTest
-    public void beforeTest() {
-        LOG.info("======================= BEFORETEST =======================");
-        String url = System.getProperty("docker.url", "http://10.117.162.153:2375");
-        LOG.info("Connecting to Docker server at " + url);
-        dockerClient = new DockerClient(url);
+    public void beforeTest() throws DockerException {
+        super.beforeTest();
     }
-
     @AfterTest
     public void afterTest() {
-        LOG.info("======================= END OF AFTERTEST =======================");
+        super.afterTest();
+    }
+
+    @BeforeMethod
+    public void beforeMethod(Method method) {
+        super.beforeMethod(method);
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult result) {
+        super.afterMethod(result);
     }
 
     @Test
     public void testDockerVersion() throws DockerException {
-        Version version = dockerClient.versionCmd().exec();
+        Version version = testClient.versionCmd().exec();
         LOG.info(version.toString());
 
         assertTrue(version.getGoVersion().length() > 0);
         assertTrue(version.getVersion().length() > 0);
 
         assertEquals(StringUtils.split(version.getVersion(), ".").length, 3);
+    }
+
+    @Test
+    public void testDockerListImages() throws DockerException {
+        List<Image> images = testClient.listImagesCmd().exec();
+        LOG.info(images.toString());
+        assertTrue(images.size() != 0);
     }
 }
